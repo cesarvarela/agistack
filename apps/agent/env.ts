61c525e6df51de57244b597252ca3e68f418f053
Cesar Varela <mail@cesarvarela.com>
2025-11-13 00:54:31 -0300

    feate: make node port configurable

--- apps/agent/env.ts
+++ apps/agent/env.ts
@@ -5,6 +5,7 @@ dotenv.config({ path: "../../.env.local" })
 
 const schema = z.object({
 	AGENT_SECRET: z.string().min(1, "AGENT_SECRET is required"),
+	NODE_PORT: z.coerce.number().default(9090),
 })
 
 export default schema.parse(process.env)
--- apps/agent/index.ts
+++ apps/agent/index.ts
@@ -1,12 +1,11 @@
 import { Node } from "@agistack/node-api"
-import { NODE_PORT } from "@agistack/node-api/constants"
 import dotenv from "dotenv"
 import env from "./env"
 
 dotenv.config({ path: "../../.env.local" })
 
 ;(async () => {
-	const node = new Node(NODE_PORT, env.AGENT_SECRET)
+	const node = new Node(env.NODE_PORT, env.AGENT_SECRET)
 
 	await node.start()
 
